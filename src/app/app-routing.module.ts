355bf964164b8736679965096ecc33e9a913818c
David <david.ds636@gmail.com>
2019-04-08 07:15:12 +0200

    inicial

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// Use RouterModule, Routes for activating routing in angular
+import { RouterModule, Routes } from '@angular/router';
+
+// Include components for in which router service to be used
+import { AddStudentComponent } from './add-student/add-student.component';
+import { StudentsListComponent } from './students-list/students-list.component';
+import { EditStudentComponent } from './edit-student/edit-student.component';
+
+// Routes array define component along with the path name for url
+const routes: Routes = [
+  { path: 'register-student', component: AddStudentComponent },
+  { path: 'view-students', component: StudentsListComponent },
+  { path: 'edit-student/:id', component: EditStudentComponent },
+  // { path: '', redirectTo: '/register-student', pathMatch: 'full' }
+  { path: '**', pathMatch: 'full', redirectTo: 'register-student' }
+];
+
+export const APP_ROUTING = RouterModule.forRoot(routes, { useHash: true });
+
+// Import RouterModule and inject routes array in it and dont forget to export the RouterModule
+@NgModule({
+  imports: [CommonModule, RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  declarations: []
+})
+export class AppRoutingModule { }
